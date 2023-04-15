642e03f474a510ed3745741ca52136516f7745ec
Meesum Abbas <79690513+meesum5445@users.noreply.github.com>
2023-04-15 07:15:04 -0700

    ..

--- LUDO CABARET/Home.cpp
+++ LUDO CABARET/Home.cpp
@@ -12,16 +12,16 @@ using namespace std;
 #include"Home.h"
 Home::Home()
 {
-	
+
 }
-Home::Home(Player* player, positionOnWindow home_coord,int home_entering_index,vector<positionOnWindow>local_path_coord, vector<positionOnWindow>room_coord,Controller_Class*controller):controller(controller),home_coordinate(home_coord),home_entering_index(home_entering_index),coordinates_of_screen_for_local_path(local_path_coord),coordinates_of_screen_for_rooms(room_coord), player(player)
+Home::Home(Player* player, positionOnWindow home_coord, int home_entering_index, vector<positionOnWindow>local_path_coord, vector<positionOnWindow>room_coord, Controller_Class* controller) :controller(controller), home_coordinate(home_coord), home_entering_index(home_entering_index), coordinates_of_screen_for_local_path(local_path_coord), coordinates_of_screen_for_rooms(room_coord), player(player)
 {
 	this->controller->homes.push_back(this);			//Adding itself to controllers class.
 	player->home = this;								//Making relation with player of this home by adding itself into player. 
 
-	local_path = vector<vector<Piece*>>(6);	
-	for(int i=0;i<this->controller->ludo_cabaret->number_of_pieces_per_home;i++)
-		this->pieces.push_back(new Piece(this,controller));				//Creating piece and pushing back them to pieces of home.
+	local_path = vector<vector<Piece*>>(6);
+	for (int i = 0; i < this->controller->ludo_cabaret->number_of_pieces_per_home; i++)
+		this->pieces.push_back(new Piece(this, controller));				//Creating piece and pushing back them to pieces of home.
 }
 void Home::display()
 {
@@ -29,19 +29,24 @@ void Home::display()
 	{
 		for (int j = 0; j < local_path[i].size(); j++)
 		{
-			local_path[i][j]->display({coordinates_of_screen_for_local_path[i].row, coordinates_of_screen_for_local_path[i].col + j * 5},{45,45});
+			local_path[i][j]->display({ coordinates_of_screen_for_local_path[i].row, coordinates_of_screen_for_local_path[i].col + j * 5 }, { 45,45 });
 		}
 	}
 	for (int i = 0; i < this->controller->ludo_cabaret->number_of_pieces_per_home; i++)
 	{
 		if (this->pieces[i]->out_of_home == false)
-			this->pieces[i]->display(coordinates_of_screen_for_rooms[i],{45,45});
+			this->pieces[i]->display(coordinates_of_screen_for_rooms[i], { 45,45 });
 	}
 }
 void Home::move_inside_home(Position source, Position destination)
 {
-	this->local_path[destination.path_index].push_back(this->local_path[source.path_index][source.floor]);
-	this->local_path[source.path_index].erase(this->local_path[source.path_index].begin() + source.floor);				// make source empty by erasing
+	for (int i = source.path_index; i < destination.path_index; i++)
+	{
+		this->local_path[i + 1].push_back(this->local_path[i][source.floor]);
+		this->local_path[i].erase(this->local_path[i].begin() + source.floor);				// make source empty by erasing
+		this->controller->ludo_cabaret->display();
+	}
+
 	if (destination.path_index == 5)
 		this->player->arrived_pieces++;
 }
