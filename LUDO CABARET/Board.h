11ea13e8d868f350c355ed58b7ee6e316fe3a7ad
Meesum Abbas <79690513+meesum5445@users.noreply.github.com>
2023-04-15 07:09:59 -0700

    added goinsidehome function

--- LUDO CABARET/Board.h
+++ LUDO CABARET/Board.h
@@ -4,27 +4,28 @@
 
 class Board
 {
-	public:
-		//......RELATIONS.......//
-		Controller_Class* controller{};			//pointer to controller class ,so board can be linked to controller.
-		vector<Home*>homes{};
-		Dice* dice{};
-		//......PERSONAL ATTRIBUTES......//
-		int number_of_homes{};
-		vector<vector<Piece*>>path{};
-		vector<vector<int>>coordinates_of_screen_for_path{};
-		//..................;
+public:
+	//......RELATIONS.......//
+	Controller_Class* controller{};			//pointer to controller class ,so board can be linked to controller.
+	vector<Home*>homes{};
+	Dice* dice{};
+	//......PERSONAL ATTRIBUTES......//
+	int number_of_homes{};
+	vector<vector<Piece*>>path{};
+	vector<vector<int>>coordinates_of_screen_for_path{};
+	//..................;
 
 
 
-	public:
-		Board();
-		Board(Controller_Class*);
-		void display();
-		void init_coordinate_of_path();
-		void move_piece(Position ,Position);
-		vector<positionOnWindow> local_path_coordinates(int);
-		vector<positionOnWindow>rooms_coodinates(int home);
-		~Board();
+public:
+	Board();
+	Board(Controller_Class*);
+	void display();
+	void init_coordinate_of_path();
+	void move_piece(Position, Position);
+	vector<positionOnWindow> local_path_coordinates(int);
+	vector<positionOnWindow>rooms_coodinates(int home);
+	void go_inside_home(Position, Position);
+	~Board();
 
 };
