1d9824cbb7eca4d38aef45f256a55f5be36e5c96
Meesum Abbas <79690513+meesum5445@users.noreply.github.com>
2023-10-07 21:36:28 +0500

    Update Controller_Class.cpp

--- LUDO CABARET/Controller_Class.cpp
+++ LUDO CABARET/Controller_Class.cpp
@@ -12,17 +12,3 @@ Controller_Class::Controller_Class()
 {
 
 }
-void Controller_Class::print()
-{
-	/*cout << this->board << "\n\n";
-	cout << this->dice << "\n\n";
-
-	for (int i = 0; i < teams.size(); i++)
-		cout << this->teams[i]<<endl;
-	cout << "\n\n\n";
-	for (int i = 0; i < this->players.size(); i++)
-		cout << this->players[i]<<"is player of"<< this->players[i]->team<< endl;
-	cout << "\n\n\n";
-	for (int i = 0; i < this->homes.size(); i++)
-		cout << this->homes[i]<<endl;*/
-}
